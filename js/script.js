432deae6f0130d3585e35ced30891f069469629d
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-10 16:20:48 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@
 var numPcArray = [];
 var numUserArray = [];
 
+
+// Generare 16 numeri per Pc(tra  1 e 100). come si fa con function e while o senza function?
+
+while (numPcArray.length <=16){
+
+  var numRandom = 1+ Math.floor(100*Math.random());
+
+
+  if (!numPcArray.includes(numRandom)){
+      numPcArray.push[numRandom];
+  }
+
+}
+
+document.getElementById('random-pc-number').innerHTML=numPcArray;
+
+
 // Di seguito, creo un prompt per chiedere all'Utente di inserire un numero (compreso tra  1 e 100) per (100-16) volte, ciò 84 volte;
 //i numeri che vengono inseriti da numUserScelta verranno salvati in numUserArray;
 
