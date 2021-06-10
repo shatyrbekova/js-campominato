d6e7ef18705bcef088316320e3610b24e1911307
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-10 15:27:02 +0200

    creati due array;

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Devo creare due array. In particolare,  per Pc e per Utente in modo che i numeri non vengono duplicati;
+var numPcArray = [];
+var numUserArray = [];
+
+// Di seguito, creo un prompt per chiedere all'Utente di inserire un numero (compreso tra  1 e 100) per (100-16) volte, ciò 84 volte;
+//i numeri che vengono inseriti da numUserScelta verranno salvati in numUserArray;
+
+while (numUserArray.length<= 84){
+  var numUserScelta = parseInt(prompt('Inserire un numero tra 1 e 100'));
+   numUserArray.push[numUserScelta];
+      if (numUser=== numUserScelta){
+
+      }
+  
+}
