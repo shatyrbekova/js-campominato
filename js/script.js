019c41e8277abd2027da550cd1371f1f51ed1274
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-11 12:17:11 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // Devo creare due array. In particolare,  per Pc e per Utente in modo che i numeri non vengono duplicati;
 var numPcArray = [];
 var numUserArray = [];
-var possibility= 2;
+var possibility= 84;
 var numUserScelta;
 
 var flag = false;
@@ -18,6 +18,8 @@ while (numPcArray.length < 16){
 
 }
 
+console.log(numPcArray);
+
 
 
 document.getElementById('random-pc-number').innerHTML= 'Il numero casuale di PC:' + '  ' + numPcArray;
@@ -25,13 +27,22 @@ document.getElementById('random-pc-number').innerHTML= 'Il numero casuale di PC:
 // Di seguito, creo un prompt per chiedere all'Utente di inserire un numero (compreso tra  1 e 100) per (100-16) volte, ciò 84 volte;
 //i numeri che vengono inseriti da numUserScelta verranno salvati in numUserArray;
 
-while (numUserArray.length <= 16 ){ //84
+while (numUserArray.length < possibility ){ //84
 
-   var numUserScelta = parseInt(prompt('Inserire un numero tra 1 e 100'));
+   numUserScelta = parseInt(prompt('Inserire un numero tra 1 e 100'));
      
    
       if (!numUserArray.includes(numUserScelta)){
-        numUserArray.push(numUserScelta)
+        
+        if(numPcArray.includes(numUserScelta)){
+          alert('Mina beccata');
+          flag = true;
+          break
+        }else {
+       
+          numUserArray.push(numUserScelta)
+          console.log(numUserArray);
+        }  
       } 
       else if (isNaN (numUserScelta)|| numUserScelta < 1 || numUserScelta > 100) {
         alert('Dovevi inserire un numero da 1 a 100');
@@ -39,13 +50,16 @@ while (numUserArray.length <= 16 ){ //84
       else  {
         alert ('Hai già inserito questo numero')
       } 
-        
-      
-
-   
 }
 
 document.getElementById('random-pc-number').innerHTML+= '<br>' + 'Il numero dell\'utente:' + ' '+numUserArray;
 
+// Come calcolare lo score del game?
 
+if (!flag){
+  alert('Hai vinto');
+} else {
+  alert ('Hai perso');
+}
 
+alert ('Il tou punteggio è ' + numUserArray);
