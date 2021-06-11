94eecad315aedc54383e9b5902fea1ff598b48d8
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-11 16:11:26 +0200

    struttura

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -6,39 +6,37 @@
 
 //Creo un prompt con parseInt per chiedere il range di numeri casuali;
 
+var numPcArray = [];
+var numUserArray = [];
+var possibility= 100-16;
+var flag = false; //flag ci serve per capire chi ha perso oppure chi ha vinto;
 var rangeUser = parseInt(prompt('Scegliere un livello di difficoltà: 0, 1 oppure 2'));
 
 switch(rangeUser){
 
     case 0: 
         var min = 1;
-        var max =100;
-        possibility = 100;
-        alert('Inserisci un numero compreso tra 1 e 100');
+        var max =100-16;
+        alert('Inserisci un numero compreso tra 1 e' + max);
         break;
 
     case 1:
-        numMin = 1;
-        numMax =80;
-        possibility = 80;
-        alert('Inserisci un numero compreso tra 1 e 80');
+        min = 1;
+        max =80-16;
+    
+        alert('Inserisci un numero compreso tra 1 e ' + max);
         break;
     
     case 2:
         min = 1;
-        max = 50;
-        possibility = 50;
-        alert('Inserisci un numero compreso tra 1 e 50');
+        max = 50-16;
+        alert('Inserisci un numero compreso tra 1 e '+ max);
         break;
 
 }
 
 // Devo creare due array. In particolare,  per Pc e per Utente in modo che i numeri non vengono duplicati;
-var numPcArray = [];
-var numUserArray = [];
-var possibility= 100-16;
-var numUserScelta;
-var flag = false; //flag ci serve per capire chi ha perso oppure chi ha vinto;
+
 
 function getRandomNumber (min,max) {
 
@@ -63,38 +61,40 @@ console.log ('COMPUTER'+numPcArray);
 
 
 
-document.getElementById('random-pc-number').innerHTML= 'Il numero casuale di PC:' + '  ' + numPcArray;
+document.getElementById('output').innerHTML+= 'Il numero casuale di PC:' + '  ' + numPcArray;
 
 // Di seguito, creo un prompt per chiedere all'Utente di inserire un numero (compreso tra  1 e 100) per (100-16) volte, ciò 84 volte;
 //i numeri che vengono inseriti da numUserScelta verranno salvati in numUserArray;
 
-while (numUserArray.length < possibility ){ //84
+while (numUserArray.length < max ){ 
 
-   numUserScelta = parseInt(prompt('Inserire un numero tra 1 e 100'));
+   var numUserScelta = parseInt(prompt('Inserire un numero tra 1 e '+max));
      
-   
-      if (!numUserArray.includes(numUserScelta)){
+   if (isNaN (numUserScelta)|| numUserScelta < 1 || numUserScelta > 100) { // PROBLEMA: Non funziona numUserScelta < 1 || numUserScelta > 100?
+    alert('Dovevi inserire un numero da 1 a 100');
+  } else {
+    if (!numUserArray.includes(numUserScelta)){ 
+        numUserArray.push(numUserScelta)
+        if(numPcArray.includes(numUserScelta)){
+            alert('Mina beccata');
+            flag = true;
+            break;
+          }
+       } else  {
+        alert ('Hai già inserito questo numero')
+      } 
         
         //se l'utente inserisse un numero simile a quello di PC, il gioco sarebbe finito ed utente avrebbe perso;
 
-        if(numPcArray.includes(numUserScelta)){
-          alert('Mina beccata');
-          flag = true;
-          break;
-        }else {
-       // se l'utente inserisse un numero che numUserArray non contiene, allora a questo punto il numero generato verrebbe salvato nell'array in questione;
-          numUserArray.push(numUserScelta)
-          console.log(numUserArray);
-        }  
-      } 
+       
+  }
+   
+    
+      
       // se l'utente inserisse non un numero, ma una parola ossia una lettera uscirebbe un alert;
-      else if (isNaN (numUserScelta)|| numUserScelta < 1 || numUserScelta > 100) { // PROBLEMA: Non funziona numUserScelta < 1 || numUserScelta > 100?
-        alert('Dovevi inserire un numero da 1 a 100');
-      }
+     
       //inoltre, se l'utente scegliesse un numero che aveva già inserito, di nuovo uscirebbe un alert;
-      else  {
-        alert ('Hai già inserito questo numero')
-      } 
+      
 }
 
 console.log('USER'+ numUserArray)
